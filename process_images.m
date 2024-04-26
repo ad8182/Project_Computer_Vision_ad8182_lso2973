b13333c7f364db161e0778d8e513fe498287b7b8
lso2973 <78745647+lso2973@users.noreply.github.com>
2024-04-26 17:00:06 -0400

    Slight refactoring to process_images.m
    Add Andrew Dantone <ad8182> to the Authors list. Additionally, make it such that the original dataset of images doesn't get destroyed when creating the images for resnetNetwork. Finally, make the output images be 224x224 for the same reason.

--- process_images.m
+++ process_images.m
@@ -9,12 +9,18 @@ function process_images
     % used with the resnet50() function in the main project itself.
     %
     % Author: Lily O'Carroll <lso2973>
-    % Date: 23 April, 2024
+    %         Andrew Dantone <ad8182>
+    % Date: 26 April, 2024
     %
 
-    % This directory contains all of the images
-    % that we need to adjust accordingly
-    image_dir = "NAZCA_SCANNED_GEMS_Original - Copy\NAZCA_SCANNED_GEMS";
+    % This is the directory where the cropped and scaled
+    % images will be saved to -- this is done so that the
+    % original dataset of gemstones doesn't get overwritten.
+    image_dir = "OUTPUT";
+
+    % Copy all of the images in the dataset (located in
+    % NAZCA_SCANNED_GEMS) to the "OUTPUT" directory.
+    copyfile NAZCA_SCANNED_GEMS OUTPUT
 
     % Get all of the image files so we can process them
     filelist = dir(fullfile(image_dir, '**\*.jpg'));
@@ -22,27 +28,18 @@ function process_images
     filelist = filelist(~[filelist.isdir]);
     
     % Go through all of the images in our testing set.
-    for i = 1 : size(filelist)
+    %%% Starting in R2024a this specific line of code will throw
+    %%% a warning, despite it working just fine.
+    for img = 1 : size(filelist)
         % Get the filename of the image, read it in,
         % and convert it into a binarized image.
-        gem_location = "" + filelist(i).folder + '\' + filelist(i).name;
+        gem_location = "" + filelist(img).folder + '\' ...
+            + filelist(img).name;
         im_preprocessed = im2double(imread(gem_location));
         im_gray = rgb2gray(im_preprocessed);
         im_bw = imbinarize(im_gray);
-        % im_bw = ~im_bw;
-        % figure();
-        % %subplot (1,3,1);
-        % imagesc(im_preprocessed);
-        % title 'preprocessed';
-        % figure();
-        % %subplot (1,3,2);
-        % imagesc(im_gray);
-        % title 'gray';
-         % figure();
-         % %subplot (1,3,3);
-         % imagesc(im_bw);
-         % title 'bw';
-         %Set up disks for dilation (disks A, C) and
+
+        % Set up disks for dilation (disks A, C) and
         % a disk for erosion (disk B).
         disk_A = strel("disk", 5);
         disk_B = strel("disk", 10);
@@ -53,19 +50,6 @@ function process_images
         im_eroded = imerode(im_dilated, disk_B);
         im_binary = imdilate(im_eroded, disk_C);
         
-         % figure();
-         % %subplot (1,3,1);
-         % imagesc(im_dilated);
-         % title 'dilated';
-         % figure();
-         % %subplot (1,3,2);
-         % imagesc(im_eroded);
-         % title 'eroded';
-        % figure();
-        % %subplot (1,3,3);
-        % imagesc(im_binary);
-        % title 'binary';
-
         % Do connected component analysis to get the region where
         % the rock is.
         [im_connected_components, number_of_cc] = bwlabel(~im_binary, 4);
@@ -78,8 +62,6 @@ function process_images
         seccond_highest_rock_x = 0;
         highest_rock_y = 0;
         seccond_highest_rock_y = 0;
-        highest_bounding_box = [0.0,0.0,0.0,0.0];
-        seccond_highest_bounding_box = [0.0,0.0,0.0,0.0];
         % For all components found in this image,
         for this_component = 0 : number_of_cc
 
@@ -91,23 +73,15 @@ function process_images
             ss = regionprops(binary_image, 'BoundingBox');
 
             % If the number of pixels in this binary region is
-            % the highest amount found so far,
-
-            %%% IDEA -- Use the second-highest amount of pixels?
-            %%% this accounts for the fact that this script only works
-            %%% well on darker-colored gemstones, and will often
-            %%% miss the lighter-colored gemstones in some capacity.
-            %%% For example, this script COMPLETELY fails with
-            %%% the moonstone dataset...
-            if (n_pix > highest_num_of_pixels || seccond_highest_num_of_pixels == 0)
+            % the second highest amount found so far,
+            if (n_pix > highest_num_of_pixels || ... 
+                    seccond_highest_num_of_pixels == 0)
                 % Set the highest number of pixels, and
                 % the "bounding box" points
                 if(n_pix > highest_num_of_pixels)
                     seccond_highest_num_of_pixels = highest_num_of_pixels;
                     highest_num_of_pixels = n_pix;
                     bounding_box_points = ss.BoundingBox;
-                    seccond_highest_bounding_box = highest_bounding_box;
-                    highest_bounding_box = bounding_box_points;
                     seccond_highest_rock_x = highest_rock_x;
                     highest_rock_x = bounding_box_points(1);
                     seccond_highest_rock_y = highest_rock_y;
@@ -115,7 +89,6 @@ function process_images
                 else
                     seccond_highest_num_of_pixels = n_pix;
                     bounding_box_points = ss.BoundingBox;
-                    seccond_highest_bounding_box = bounding_box_points;
                     seccond_highest_rock_x = bounding_box_points(1);
                     seccond_highest_rock_y = bounding_box_points(2);
                 end
@@ -126,33 +99,20 @@ function process_images
         % points found earlier to crop it for resnet50()
         % (This is why the image size is 672x672 -- it
         % will be shrunken down to 224x224)
-        % hold on;
-        % box = seccond_highest_bounding_box;
-        %  x1 = [box(1), (box(1) + box(3))];
-        %  y1 = [box(2), box(2)];
-        %  x2 = [box(1), box(1)];
-        %  y2 = [box(2), (box(2) + box(4))];
-        %  x3 = [box(1), (box(1) + box(3))];
-        %     y3 = [(box(2) + box(4)),(box(2) + box(4))];
-        %     x4 = [(box(1) + box(3)),(box(1) + box(3))];
-        %     y4 = [box(2), (box(2) + box(4))];
-        %     %draw the box
-        %     pause(3);
-        %     hold on;
-        %     plot(x1, y1, 'LineWidth', 1, 'Color', [0.5, 0, 0.5]);
-        %     plot(x2, y2, 'LineWidth', 1, 'Color', [0.5, 0, 0.5]);
-        %     plot(x3, y3, 'LineWidth', 1, 'Color', [0.5, 0, 0.5]);
-        %     plot(x4 ,y4, 'LineWidth', 1, 'Color', [0.5, 0, 0.5]);
-
         final_image = imcrop(im_preprocessed, ... 
-            [(seccond_highest_rock_x-84) (seccond_highest_rock_y-84) 672 672]);
-        % figure();
-        % %subplot (1,3,1);
-        % imagesc(final_image);
-        % title 'final';
-        % Write the image in that file location
-        imwrite(final_image, gem_location);
+            [(seccond_highest_rock_x-84) (seccond_highest_rock_y-84) ...
+            672 672]);
+
+        % Forcibly resize our image to be 224x224
+        % for use with resnetNetwork
+        resized_final_image = imresize(final_image, [224 224]);
+
+        % Write the final image to disk, in
+        % the new directory (so that the original
+        % training set of images doesn't get
+        % overwritten)
+        imwrite(resized_final_image, gem_location);
 
     end
-
+    fprintf("Done processing images\n");
 end
